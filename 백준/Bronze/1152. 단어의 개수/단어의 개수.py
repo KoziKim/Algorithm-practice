22b00053d5b913ee302eb5a35c06fe35dcf68406
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-08 17:36:38 +0900

    [Bronze II] Title: 단어의 개수, Time: 60 ms, Memory: 39196 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,22 +1,2 @@
-A = input()
-N = len(A)
-ans = 0
-
-for i in range(0, N):
-    if N == 1:
-        if A[0] == " ":
-            print(0)
-            break
-        else:
-            print(1)
-            break
-    if i == N - 1:
-        print(ans + 1)
-        break
-    if A[0] == " ":
-        i = i + 1
-    if A[i] == " ":
-        if i == N - 1:
-            print(ans + 1)
-            break
-        ans = ans + 1
+N = list(input().split())
+print(len(N))
