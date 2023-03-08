594b35d438fe5b6ae7fda0866704b7945162a459
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-08 22:44:20 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,29 +1,3 @@
-arr = [-1]*26
-j = 0
-A = input()
-alpha = [chr(i) for i in range(97, 123)]
-
-word = []
-for i in range(len(A)):
-    word.append(A[i])
-
-for i in range(len(word)):
-    j = 0
-    while alpha[j]:
-        if j == 25:
-            if alpha[j] == word[i]:
-                arr[j] = i
-                alpha[j] = "A"
-                break
-            else:
-                break
-        if alpha[j] == word[i]:
-            arr[j] = i
-            alpha[j] = "A"
-        j += 1
-
-for i in range(len(arr)):
-    if i == len(arr)-1:
-        print(arr[i], end ='')
-        exit()
-    print(arr[i], end=' ')
+s = input()
+for i in range(97,123):
+    print(s.find(chr(i)), end=' ')
