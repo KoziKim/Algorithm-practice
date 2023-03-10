7c48458f2a55ba174f9570db06e0359639a5e902
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-10 14:28:18 +0900

    [Silver I] Title: 괄호의 값, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,40 @@
+import sys
+bracket = sys.stdin.readline().rstrip()
+S = []
+ans = 0
+tmp = 1
+
+def listrindex(a, value):
+    return len(a) - a[-1::-1].index(value) -1
+
+for i in range(len(bracket)):
+    if bracket[i] == '(':
+        S.append(bracket[i])
+        tmp *= 2
+    elif bracket[i] == '[':
+        S.append(bracket[i])
+        tmp *= 3
+    elif bracket[i] == ')':
+        if '(' in S and S[-1] != '[':
+            if bracket[i-1] == '(':
+                ans += tmp
+            a = listrindex(S, '(')
+            S.pop(a)
+            tmp //= 2
+        else:
+            ans = 0
+            break
+    elif bracket[i] == ']':
+        if '[' in S and S[-1] != '(':
+            if bracket[i-1] == '[':
+                ans += tmp
+            a = listrindex(S, '[')
+            S.pop(a)
+            tmp //= 3
+        else:
+            ans = 0
+            break
+if S:
+    print(0)
+else:
+    print(ans)
