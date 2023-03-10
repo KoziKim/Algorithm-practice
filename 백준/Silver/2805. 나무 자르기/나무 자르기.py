6a23a141f59a7fb223b57c515ec5964c6a74984f
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-10 21:07:37 +0900

    [Silver II] Title: 나무 자르기, Time: 2076 ms, Memory: 149680 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,18 +1,28 @@
-N, M = map(int, input().split())
-
-q = []
-treeH = list(map(int, input().split()))
-left, right = 1, max(treeH)  # 이진 탐색을 위한 변수 설정
-
-while left <= right:  # 이진 탐색 시작
-    mid = (left + right) // 2
+def cut(trees, mid):
+    # mid 길이로 나무들을 잘랐을 때 얻을 수 있는 나무의 길이 합을 계산
     total = 0
-    for h in treeH:
-        if h - mid > 0:
-            total += h - mid
-    if total < M:
-        right = mid - 1
-    else:
-        left = mid + 1
+    for tree in trees:
+        if tree > mid:
+            total += tree - mid
+    return total
 
-print(right)
+def binary_search(trees, target):
+    left, right = 1, max(trees) # left는 최소 1, right은 나무 중 가장 긴 길이로 설정
+    result = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+        total = cut(trees, mid)
+
+        if total >= target: # 더 많은 길이를 얻을 수 있는 경우
+            result = mid
+            left = mid + 1
+        else: # 더 적은 길이를 얻을 수 있는 경우
+            right = mid - 1
+
+    return result
+
+n, target = map(int, input().split())
+trees = list(map(int, input().split()))
+
+print(binary_search(trees, target))
