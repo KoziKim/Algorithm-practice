ecec50b457713e2bab43684d9bacfa947bfa13ad
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-17 16:10:28 +0900

    [Gold V] Title: 이진 검색 트리, Time: 2156 ms, Memory: 33684 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -12,13 +12,13 @@ while True:
 def postorder(root, end): #0, 8
     if root > end:
         return
-    mid = end + 1 #9 #6
-    for i in range(root + 1, end + 1): #1, 9 #1, 6
-        if nums[i] > nums[root]: #i번째 수가 루트 노드보다 더 크면,
-            mid = i
+    mid = end + 1 #9
+    for i in range(root + 1, end + 1): #1, 9 (1~8)
+        if nums[i] > nums[root]: # i번째 수가 루트 노드보다 더 크면,
+            mid = i # 큰 수 해당하는 인덱스 mid에 넣음
             break
-    postorder(root + 1, mid - 1) #1, 5
-    postorder(mid, end) #6, 8
-    print(nums[root])
+    postorder(root + 1, mid - 1) # 왼쪽 (작은 수)
+    postorder(mid, end) # 오른쪽 (큰 수)
+    print(nums[root]) # 루트
 
 postorder(0, len(nums)-1)
