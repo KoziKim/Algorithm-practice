44b9d0d087f383a89ff60f533439a5913a2aa714
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-21 22:49:41 +0900

    [Gold III] Title: 줄 세우기, Time: 208 ms, Memory: 38872 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,16 +3,13 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = []
 inDegree = [0 for i in range(n+1)]
 graph = [[] for i in range(n+1)]
 q = deque()
+ans = []
 
 for i in range(m):
     a, b = map(int, input().split())
-    arr.append([a, b])
-
-for a, b in arr:
     graph[a].append(b)
     inDegree[b] += 1
 
@@ -22,8 +19,10 @@ for i in range(1, n + 1):
 
 while q:
     student = q.popleft()
+    ans.append(student)
     for j in graph[student]:
         inDegree[j] -= 1
         if inDegree[j] == 0:
             q.append(j)
-    print(student, end = ' ')
+
+print(*ans)
