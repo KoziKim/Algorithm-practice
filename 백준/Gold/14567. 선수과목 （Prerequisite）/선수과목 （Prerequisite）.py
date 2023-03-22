5f4aa2d392722fb88a930d8a236d7e025edc1429
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-23 00:04:56 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 656 ms, Memory: 50524 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -1,15 +1,29 @@
 import sys
+import collections
+from collections import deque
+
 input = sys.stdin.readline
 
-N, M = map(int,input().split())
-graph = [1]*(N+1)
-arr = [[] for _ in range(N+1)]
-
-for i in range(M):
-    a,b = map(int,input().split())
+n, m = map(int, input().split())
+cur = [0 for i in range(n + 1)]
+res = [0 for i in range(n + 1)]
+dq = deque()
+arr = collections.defaultdict(list)
+for i in range(m):
+    a, b = map(int, input().split())
     arr[a].append(b)
+    res[b] += 1
 
-for i in range(1, N+1):
-    for b in arr[i]:
-        graph[b] = max(graph[b], graph[i]+1)
-print(*graph[1:])
+for i in range(1, n + 1):
+    if res[i] == 0:  #진입차수가 0 이면
+        dq.append((i, 1))  #진입차수가 0 인것을 dq에 삽입
+        cur[i] = 1  #1학기에 바로 이수 가능
+
+while dq:
+    a, cnt = dq.popleft()
+    for i in arr[a]:
+        res[i] -= 1
+        if res[i] == 0:
+            dq.append((i, cnt + 1))
+            cur[i] = cnt + 1
+print(*cur[1:])
