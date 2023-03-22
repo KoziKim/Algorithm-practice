a19b9670a903ee9c842f1fb1ad6330f64f6e1a56
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-22 23:24:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,21 @@
+from collections import deque
+import sys
+
+N, K = map(int, sys.stdin.readline().rstrip().split())
+
+q = deque()
+q.append(N)
+MAX = 10**5
+check = [0] * (MAX + 1)
+
+while q:
+    x = q.popleft()
+    if x == K:
+        print(check[x])
+        break
+    for nx in (x+1, x-1, 2*x):
+        if nx < 0 or nx > MAX: 
+            continue
+        if 0 <= nx <= nx and check[nx] == 0:
+            check[nx] = check[x] + 1
+            q.append(nx)
