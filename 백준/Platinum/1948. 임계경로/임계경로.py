e9a358994215d3482321187caa5d19ab61329d2b
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-03-22 22:12:15 +0900

    [Platinum V] Title: 임계경로, Time: 220 ms, Memory: 42736 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.py
+++ 백준/Platinum/1948. 임계경로/임계경로.py
@@ -8,14 +8,12 @@ M = int(input())
 
 indegree = [0] * (N+1)
 graph = [[] for _ in range(N+1)]
-# return_graph = [[] for _ in range(N+1)]
-cnt = [[] for _ in range(N+1)]
+back = [[] for _ in range(N+1)]
 
 for _ in range(M):
     # a: 출발 도시, b: 도착 도시, c: 걸리는 시간
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
-    # return_graph[b].append(a)
     indegree[b] += 1
 
 start, end = map(int, input().split())
@@ -26,31 +24,30 @@ q.append(start)
 time = [0] * (N+1)
 
 while q:
-    # now: 도시, c: 지나온 경로의 개수
+    # now: 도시, back: 직전 경로(경로 추적 위함)
     now = q.popleft()
 
-    for (city, cost) in graph[now]:
-        indegree[city] -= 1
-        # 비용이 갱신 될 때
-        if time[city] < time[now] + cost:
-            time[city] = time[now] + cost
-            # 달려야 하는 도로의 수 갱신
-            cnt[city] = [now]
-        elif time[city] == time[now] + cost:
-            cnt[city].append(now)
-
-        # 선행 도로를 모두 지나갔을 때
-        if indegree[city] == 0:
-            q.append(city)
+    for (next, cost) in graph[now]:
+        indegree[next] -= 1
+        if indegree[next] == 0:
+            q.append(next)
+        if time[next] < time[now] + cost:
+            # 더 오래 걸리는 걸로 갱신
+            time[next] = time[now] + cost
+            # 직전 경로도 갱신
+            back[next] = [now]
+        # 시간이 같을 경우(같은 시간 경로 여러개일 경우), 추가함.
+        elif time[next] == time[now] + cost:
+            back[next].append(now)
 
 q = deque([end])
 route = set()
 while q:
     now = q.popleft()
-    for x in cnt[now]:
-        if (now, x) not in route:
-            route.add((now, x))
-            q.append(x)
+    for nx in back[now]:
+        if (now, nx) not in route:
+            route.add((now, nx))
+            q.append(nx)
 
 print(time[end])
 print(len(route))
