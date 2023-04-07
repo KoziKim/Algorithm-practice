a808a9eb68d2fed693e1390706085e72d6aa9e14
KoziKim <126449657+KoziKim@users.noreply.github.com>
2023-04-07 23:40:49 +0900

    [Bronze IV] Title: Archivist, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,8 @@
+y = int(input())
+
+if y == 2006:
+    print('PetrSU, ITMO')
+elif 1996<=y<=1997 or y == 2000 or 2007<=y<=2008 or y == 2013 or y == 2018:
+    print('SPbSU')
+else:
+    print('ITMO')
