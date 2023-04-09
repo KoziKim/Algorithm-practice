744af77dca7d92525c52024231f340bc956506b9
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-04-10 00:10:32 +0900

    [Bronze IV] Title: Counting Clauses, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+# a = b = c = 0
+# for i in range(n):
+#     x, y, z = map(int, input().split())
+#     a += x
+#     b += y
+#     c += z
+
+if n >= 8:
+    print('satisfactory')
+else:
+    print('unsatisfactory')
