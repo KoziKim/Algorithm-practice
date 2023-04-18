987ec91bb48546205ba37293110480c19779cbdc
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-04-18 22:27:36 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,31 @@
+while True:
+    a = input()
+    if a == "TTYL":
+        print("talk to you later")
+        break
+    elif a == "CU":
+        print("see you")
+    elif a == ":-)":
+        print("I’m happy")
+    elif a == ":-(":
+        print("I’m unhappy")
+    elif a == ";-)":
+        print("wink")
+    elif a == ":-P":
+        print("stick out my tongue")
+    elif a == "(~.~)":
+        print("sleepy")
+    elif a == "TA":
+        print("totally awesome")
+    elif a == "CCC":
+        print("Canadian Computing Competition")
+    elif a == "CUZ":
+        print("because")
+    elif a == "TY":
+        print("thank-you")
+    elif a == "YW":
+        print("you’re welcome")
+    elif a == "TTYL":
+        print("talk to you later")
+    else:
+        print(a)
