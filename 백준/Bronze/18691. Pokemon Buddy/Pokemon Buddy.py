490327612348631930cebc1a67fb694d3301ca4b
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-04-18 23:52:49 +0900

    [Bronze IV] Title: Pokemon Buddy, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/18691. Pokemon Buddy/Pokemon Buddy.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(t):
+    g, c, e = map(int, input().split())
+    if c >= e:
+        print(0)
+    elif g == 1:
+        print(e - c)
+    elif g == 2:
+        print((e - c) * 3)
+    elif g == 3:
+        print((e - c) * 5)
