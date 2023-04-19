c515fdb0d2425c08750963b4112c8a182b2d1a70
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-04-19 20:26:02 +0900

    [Bronze IV] Title: Rulltrappa, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/20867. Rulltrappa/Rulltrappa.py
@@ -0,0 +1,11 @@
+m, s, g = map(int, input().split())
+
+a, b = map(float, input().split())
+
+l, r = map(int, input().split())
+
+
+if m/g + l/a > m/s + r/b:
+    print("latmask")
+elif m/g + l/a < m/s + r/b:
+    print("friskus")
