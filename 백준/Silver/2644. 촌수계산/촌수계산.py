3cf34ac6244cac5e20251cac93876797216e3362
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-04-19 10:56:56 +0900

    [Silver II] Title: 촌수계산, Time: 84 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n = int(input())
+a, b = map(int, input().split())
+
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+check = [False for _ in range(n+1)]
+
+for i in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+# for i in graph[a]:
+#     print(i)
+#     print(graph[i])
+# print(graph[a])
+
+q = deque()
+q.append((a, 0))
+f = 0
+while q:
+    me, chon = q.popleft()
+    for person in graph[me]:
+        if check[person] == True:
+            continue
+        if check[person] == False:
+            check[person] = True
+            q.append((person, chon + 1))
+        if person == b:
+            print(chon + 1)
+            f = 1
+            break
+    if f == 1:
+        break
+else:
+    print(-1)
