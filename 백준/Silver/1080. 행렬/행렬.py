d3d3810cf7b55355a11f38f1f9d6a80e68ce87de
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-04-28 10:48:01 +0900

    [Silver I] Title: 행렬, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -0,0 +1,29 @@
+row, col = map(int, input().split())
+
+graph_1 = [list(map(int, input())) for _ in range(row)]
+graph_2 = [list(map(int, input())) for _ in range(row)]
+
+cnt = 0
+
+def flip(r, c):
+    for i in range(r, r+3):
+        for j in range(c, c+3):
+            if graph_1[i][j] == 1:
+                graph_1[i][j] = 0
+            elif graph_1[i][j] == 0:
+                graph_1[i][j] = 1
+
+if row >= 3 and col >= 3:
+    for r in range(row-2):
+        for c in range(col-2):
+            if graph_1[r][c] != graph_2[r][c]:
+                flip(r, c)
+                cnt+=1
+else:
+    if graph_1 != graph_2:
+        cnt = -1
+
+if graph_1 != graph_2:
+    cnt = -1
+
+print(cnt)
