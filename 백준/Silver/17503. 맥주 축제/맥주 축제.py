bcd6b5f08d6963c284a0a647fd3aa3589a7a6aa9
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-05-03 21:01:44 +0900

    [Silver II] Title: 맥주 축제, Time: 896 ms, Memory: 79780 KB -BaekjoonHub

---
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -0,0 +1,32 @@
+import sys
+from heapq import heappop, heappush
+
+# n 축제 열리는 기간, 선호도 합 m, 마실 수 있는 맥주 종류 수 k
+
+n, m, k = map(int, input().split())
+
+# k개 줄, 선호도 v 도수 레벨 c
+beers = []
+
+sum_v = 0
+for i in range(k):
+    v, c = map(int, sys.stdin.readline().split())
+    beers.append([v, c])
+beers.sort(key=lambda x: (x[1], x[0]))
+
+candi = []
+
+liver_min = 1e9
+
+for i in range(k):
+    v, c = beers[i]
+    heappush(candi, (v, c))
+    sum_v += v
+    if len(candi) == n:
+        if sum_v >= m:
+            print(c)
+            exit()
+        else:
+            sum_v -= heappop(candi)[0]
+else:
+    print(-1)
