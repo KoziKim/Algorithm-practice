bac5c1429aa1fbb5d6601d3af0a479d84a2d0c3c
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-05-06 22:11:16 +0900

    [Gold III] Title: 다리 만들기, Time: 1996 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,12 +1,12 @@
 from collections import deque
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
 n = int(input())
 start_point = []
 graph = []
-ans = 1e9
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+i_num = set()
 
 for i in range(n):
     graph.append(list(map(int, input().split())))
@@ -14,9 +14,10 @@ for i in range(n):
         if graph[i][j] == 1:
             start_point.append((i,j))
 
-check = [[False]*n for _ in range(n)]
 q = deque()
-islands = set()
+ans = 1e9
+
+check = [[False]*n for _ in range(n)]
 
 for i in range(len(start_point)):
     x, y = start_point[i]
@@ -24,9 +25,8 @@ for i in range(len(start_point)):
         continue
     check[x][y] = True
     graph[x][y] = i + 1
-    islands.add(i+1)
+    i_num.add(i+1)
     q.append((x, y))
-
     while q:
         x, y = q.popleft()
         for j in range(4):
@@ -36,13 +36,14 @@ for i in range(len(start_point)):
                 continue
             check[nx][ny] = True
             graph[nx][ny] = i + 1
-            islands.add(i+1)
+            i_num.add(i+1)
             q.append((nx,ny))
+
+# print(graph)
 
 def find_shortest_bridge(start):
     global ans
     check = [[False]*n for _ in range(n)]
-
     d = deque()
     for i in range(len(start_point)):
         x, y = start_point[i]
@@ -50,6 +51,10 @@ def find_shortest_bridge(start):
             d.append((x, y, 0))
         else:
             continue
+    # for i in range(n):
+    #     for j in range(n):
+    #         if graph[i][j] == start:
+    #             q.append((i, j, 0))
 
     check = [[False]*n for _ in range(n)]
 
@@ -57,16 +62,18 @@ def find_shortest_bridge(start):
         x, y, length = d.popleft()
         for j in range(4):
             nx, ny = x + dx[j], y + dy[j]
-            if nx >= n or ny >= n or nx < 0 or ny < 0 or check[nx][ny] == True \
+            if nx >= n or ny >= n or nx < 0 or ny < 0 \
+                or check[nx][ny] == True \
                 or (graph[x][y] != 0 and (graph[x][y] == graph[nx][ny])):
                 continue
             if graph[nx][ny] != 0 and graph[nx][ny] != start:
                 ans = min(ans, length)
                 return
+
             check[nx][ny] = True
             d.append((nx, ny, length + 1))
 
-for island in islands:
-    find_shortest_bridge(island)
+for num in i_num:
+    find_shortest_bridge(num)
 
 print(ans)
