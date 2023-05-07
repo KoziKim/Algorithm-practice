5c37c7d7a61ec7e2fa464a3bcca99ef3cca8bb90
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-05-07 15:27:36 +0900

    [Bronze III] Title: POBEDA-2014, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/21507. POBEDA－2014/POBEDA－2014.py
+++ 백준/Bronze/21507. POBEDA－2014/POBEDA－2014.py
@@ -1,10 +1,6 @@
+from math import isqrt
 a, b, c, d = map(int, input().split())
 
-
 num_sqrs = min(a, b) + min(c, d)
-i = 0
-while True:
-    if i**2 <= num_sqrs < (i+1)**2:
-        print(i)
-        break
-    i += 1
+
+print(int(isqrt(num_sqrs)))
