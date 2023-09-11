339cbfcc5ce04fd1347bd920f593082568e673bd
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-09-11 23:41:06 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 2.53 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,9 +1,6 @@
 function solution(arr, queries) {
-    for (let i = 0; i < queries.length; i++) {
-        let [a, b] = queries[i];
-        for (let j = a; j < b+1; j++) {
-            arr[j] += 1;
-        }
-    }
+    queries.forEach(([s, e]) => {
+        while (s <= e) arr[s++]++;        
+    })
     return arr;
 }
