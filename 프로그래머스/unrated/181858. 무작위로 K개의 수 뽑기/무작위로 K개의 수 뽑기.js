255347402d1626e406fb16bfe94c508459541954
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-09-29 19:35:53 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 2.49 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,9 +1,7 @@
 function solution(arr, k) {
     let set1 = new Set(arr);
     let tmp = Array.from(set1);
-    let answer = new Array();
-    answer.length = k;
-    answer.fill(-1);
+    let answer = Array(k).fill(-1);
     tmp.map((x, i) => i < k ? answer[i] = x : x);
     return answer;   
 }
