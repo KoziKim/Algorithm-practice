37675a458d24af0fb644c336a005385d3114a5af
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-10-11 17:02:58 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,10 @@
+function solution(date1, date2) {
+    let [a, b, c, d, e, f] = [...date1, ...date2];
+    [a, b, c, d, e, f] = [+a, +b, +c, +d, +e, +f];
+    if (a < d) return 1;
+    if (a === d){
+        if (b < e) return 1;
+        if (b === e && c < f) return 1;   
+    }
+    return 0;
+}
