017e59d4dfb16a006ae413d20c2ed2c7f2e2acab
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-10-12 15:25:50 +0900

    [level unrated] Title: 커피 심부름, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,3 +1,4 @@
 function solution(order) {
-    return order.map(x => x.replaceAll(/(hot|ice)/g, "")).map(x => x === "cafelatte" ? 5000 : 4500).reduce((a, b) => a + b, 0);
+    // return order.map(x => x.replaceAll(/(hot|ice)/g, "")).map(x => x === "cafelatte" ? 5000 : 4500).reduce((a, b) => a + b, 0);
+    return order.map(x => x.includes("latte") ? 5000 : 4500).reduce((a,b) => a + b, 0);
 }
