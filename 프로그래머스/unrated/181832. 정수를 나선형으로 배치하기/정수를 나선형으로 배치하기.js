1f65f8f7213138eab81143ca43ae7331bf8cf7bb
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-10-17 21:04:16 +0900

    [level unrated] Title: 정수를 나선형으로 배치하기, Time: 0.45 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,6 +1,5 @@
 function solution(n) {
     let answer = Array.from(Array(n), () => Array(n).fill(0));
-    let cnt = n**2;
     const num = n**2;
     let tmp = n;
     let j = 0;
@@ -10,25 +9,21 @@ function solution(n) {
         for (let i = j; i < n-j; i++){
             if (answer[j][i] === 0) answer[j][i] = value;
             value++;
-            cnt--;
         }
         value--;
         for (let i = j; i < n-j; i++){
             if (answer[i][n-1-j] === 0) answer[i][n-1-j] = value;
             value++;
-            cnt--;
         }
         value--;
         for (let i = j; i < n-j; i++){
             if (answer[n-1-j][n-1-i] === 0) answer[n-1-j][n-1-i] = value;
             value++;
-            cnt--;
         }
         value--;
         for (let i = j; i < n-j; i++){
             if (answer[n-1-i][j] === 0) answer[n-1-i][j] = value;
             value++;
-            cnt--;
         }
         value--;
         j++;
