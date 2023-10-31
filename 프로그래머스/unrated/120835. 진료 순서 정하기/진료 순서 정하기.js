ed7c7a35e4b31021a3bf4c0f1eaf604c9177bc95
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-10-31 21:50:49 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,7 +1,10 @@
 function solution(emergency) {
-    emergency.map((_, i) => emergency[i] += 10);
-    for (let i = 0; i < emergency.length; i++){
-        emergency[emergency.indexOf(Math.max(...emergency))] = i+1;
-    }
-    return emergency;
+    // emergency.map((_, i) => emergency[i] += 10);
+    // for (let i = 0; i < emergency.length; i++){
+    //     emergency[emergency.indexOf(Math.max(...emergency))] = i+1;
+    // }
+    // return emergency;
+    
+    let sortedEmer = [...emergency].sort((a,b) => b - a);
+    return emergency.map(x => sortedEmer.indexOf(x) + 1);
 }
