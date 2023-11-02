dd3fb4a93246be7b654b305d9939eecfcee4fd7b
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-11-03 00:15:43 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.js
@@ -1,9 +1,15 @@
 function solution(rsp) {
-    rsp = rsp.replaceAll("2", "3");
-    rsp = rsp.replaceAll("0", "1");
-    rsp = rsp.replaceAll("5", "6");
-    rsp = rsp.replaceAll("3", "0");
-    rsp = rsp.replaceAll("1", "5");
-    rsp = rsp.replaceAll("6", "2");
-    return rsp;
+    // rsp = rsp.replaceAll("2", "3");
+    // rsp = rsp.replaceAll("0", "1");
+    // rsp = rsp.replaceAll("5", "6");
+    // rsp = rsp.replaceAll("3", "0");
+    // rsp = rsp.replaceAll("1", "5");
+    // rsp = rsp.replaceAll("6", "2");
+    // return rsp;
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2,
+    }
+    return [...rsp].reduce((a, b) => a + arr[b], "");
 }
