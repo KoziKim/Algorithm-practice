6318ea0fe889072b8551363c7ad650f9e99cefd6
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-11-12 00:25:59 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
@@ -1,4 +1,5 @@
 function solution(my_string) {
-    const regex = new RegExp(/[^aeiou]/);
-    return [...my_string].filter(x => regex.test(x)).join('');
+    // const regex = new RegExp(/[^aeiou]/);
+    // return [...my_string].filter(x => regex.test(x)).join('');
+    return my_string.replace(/[aeiou]/g, '');
 }
