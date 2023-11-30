1c0c76aefd88eb47590a60425da724697c100f65
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-11-30 23:42:18 +0900

    [level unrated] Title: 369게임, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120891. 369게임/369게임.js
+++ 프로그래머스/unrated/120891. 369게임/369게임.js
@@ -1,5 +1,3 @@
 function solution(order) {
-    let answer = 0;
-    [...order.toString()].map(x => x === "3" || x === "6" || x === "9" ? answer += 1 : "");
-    return answer;
+    return (''+order).split(/[369]/).length-1;
 }
