728462e2b25f7a9234d8e35f8e2b3edac7658c4a
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2023-12-05 01:57:02 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    let answer = '';
-    [...my_string].map((x, i) => 65 <= my_string.charCodeAt(i) && my_string.charCodeAt(i) <= 90 ? answer += String.fromCharCode(my_string.charCodeAt(i) + 32) : answer += String.fromCharCode(my_string.charCodeAt(i) - 32 ));
-    return answer;
+    return [...my_string].map(x => x === x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()).join('');
 }
